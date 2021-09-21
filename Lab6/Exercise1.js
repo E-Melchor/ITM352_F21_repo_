09e8c7903072663a1d5f7572cd9da0f8927e5af9
E-Melchor <emi96765@hawaii.edu>
2021-09-21 11:48:22 -1000

    Lab6

---
+++ Lab6/Exercise1.js
@@ -0,0 +1,10 @@
+var age =  20;
+var fav_num = 101;
+var day_of_birth = 29;
+var month_of_birth = 1;
+
+console.log(age + fav_num / day_of_birth * month_of_birth);
+
+console.log((age + fav_num) / day_of_birth * month_of_birth);
+
+console.log(1.2 + "34" + 01067 * true)
