990a6eca65a1d440d130104426c6098b42f4d8fe
E-Melchor <emi96765@hawaii.edu>
2021-11-15 10:57:02 -1000

    Update info_server_Ex2c.js

--- Lab13/info_server_Ex2c.js
+++ Lab13/info_server_Ex2c.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static('./public'));
+
 app.all('*', function (request, response, next) {
     console.log(request.method + ' to path ' + request.path + ' query string ' + JSON.stringify(request.query));
     next();
@@ -10,6 +12,5 @@ app.get('/test', function (request, response, next) {
     response.send(request.method + ' to path ' + request.path + ' query string ' + JSON.stringify(request.query));
 });
 
-app.use(express.static('./public'));
 
 app.listen(8080, () => console.log(`listening on port 8080`)); // note the use of an anonymous function here to do a callback
