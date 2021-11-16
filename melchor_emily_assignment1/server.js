e72b7e1355bd7aeb7e8f9e08d2a2cb3b52a665a0
E-Melchor <emi96765@hawaii.edu>
2021-11-15 17:46:24 -1000

    Assignment1

--- melchor_emily_assignment1/server.js
+++ melchor_emily_assignment1/server.js
@@ -5,52 +5,51 @@ app.use(express.urlencoded({ extended: true }));
 //products data
 var products = require('./product_data.json');
 app.get("/product_data.js", function (request, response) {
-   response.type('.js');
-   var products_str = `var products = ${JSON.stringify(products)};`;
-   response.send(products_str);
+    response.type('.js');
+    var products_str = `var products = ${JSON.stringify(products)};`;
+    response.send(products_str);
 });
 
 // Routing 
 
 // monitor all requests
 app.all('*', function (request, response, next) {
-   console.log(request.method + ' to ' + request.path);
-   next();
+    console.log(request.method + ' to ' + request.path);
+    next();
 });
 
 // process purchase request (validate quantities, check quantity available)
 products.forEach((prod, i) => { prod.total_sold = 0 });
 
 app.post("/purchase", function (request, response, next) {
-   let flavor = products[0]['flavor'];
-   let flavor_price = products[0]['price'];
-   var q = request.body['quantity${i}'];
+    for (i in products) {
+        var q = request.body[`quantity_textbox${i}`];
+        if (typeof q != 'undefined') {
+            if (isNonNegInt(q)) {
+                products[i].total_sold += Number(q);
+                response.redirect('invoice.html?quantity=' + q);
+            }
+            else {
+                response.redirect('invoice.html?error=Invalid%20Quantity&quantity_textbox=' + q);
+            }
+        }
+    }
 
-   if (typeof q != 'undefined') {
-       if (isNonNegInt(q)) {
-           products[0].total_sold += Number(q);
-           response.redirect(`invoice.html?quantity=` + q);
-       }
-       else {
-           response.redirect(`invoice.html?error=Invalid%20Quantity&quantity${i}=` + q);
-       }
-   }
-   response.send(request.body); 
-   //send to invoice.html?????
-   next();
+    response.send(request.body);
+    next();
 });
 
 function isNonNegInt(q, returnErrors = false) {
-   //If returnErrors is true, array of errors is returned
-   //others return true if q is a non-neg int.
-   errors = []; // assume no errors at first
-   if (q == '') q = 0;
-   if (Number(q) != q) errors.push('Not a number!'); // Check if string is a number value
-   else {
-       if (q < 0) errors.push('Negative value!'); // Check if it is non-negative
-       if (parseInt(q) != q) errors.push('Not an integer!'); // Check that it is an integer
-   }
-   return returnErrors ? errors : (errors.length == 0);
+    //If returnErrors is true, array of errors is returned
+    //others return true if q is a non-neg int.
+    errors = []; // assume no errors at first
+    if (q == '') q = 0;
+    if (Number(q) != q) errors.push('Not a number!'); // Check if string is a number value
+    else {
+        if (q < 0) errors.push('Negative value!'); // Check if it is non-negative
+        if (parseInt(q) != q) errors.push('Not an integer!'); // Check that it is an integer
+    }
+    return returnErrors ? errors : (errors.length == 0);
 };
 
 // route all other GET requests to files in public 
