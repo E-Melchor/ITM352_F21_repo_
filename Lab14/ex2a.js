9b2076dedc2494b26428aad85d6ffeb8ceed79c8
E-Melchor <emi96765@hawaii.edu>
2021-11-18 11:58:10 -1000

    Lab14

--- Lab14/ex2a.js
+++ Lab14/ex2a.js
@@ -4,8 +4,8 @@ if (filename = './user_data.json') {
     var user_data_str = fs.readFileSync(filename, 'utf-8');
     var user_data_obj = JSON.parse(user_data_str);
     var files_stats = fs.statSync(filename);
-    console.log(user_data_obj["kazman"]["password"]);
-}
-else {
+    //console.log(user_data_obj["kazman"]["password"]);
+    console.log(filename + 'has' + stats["size"] + 'characters');
+} else {
     console.log.lg(`Hey! ${filename} does not exist!`);
 }
---
+++ Lab14/ex3.js
@@ -0,0 +1,54 @@
+const fs = require('fs');
+var express = require('express');
+const { join } = require('path');
+const { json } = require('express');
+var app = express();
+
+if (filename = './user_data.json') {
+    var stats = fs.statSync(filename);
+    //console.log(user_data_obj["kazman"]["password"]);
+    console.log(filename + 'has' + stats["size"] + 'characters');
+    let data_str = fs.readFileSync(filename, 'utf-8');
+    var user_registration_info = JSON.parse(data_str);
+    console.log(user_registration_info);
+} else {
+    console.log.lg(`Hey! ${filename} does not exist!`);
+}
+
+//data from form will be decoded
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/login", function(request, response) {
+    // Give a simple login form
+    str = `
+<body>
+<form action="" method="POST">
+<input type="text" name="username" size="40" placeholder="enter username" ><br />
+<input type="password" name="password" size="40" placeholder="enter password"><br />
+<input type="submit" value="Submit" id="submit">
+</form>
+</body>
+    `;
+    response.send(str);
+});
+
+app.post("/login", function(request, response) {
+    // Process login form POST and redirect to logged in page if ok, back to login page if not
+    let login_username = request.body['username'];
+    let login_password = request.body['password'];
+    //check if username exists
+
+    if (typeof user_registration_info[login_username] != 'undefined') {
+        if (typeof user_registration_info[login_username]["password"] == login_password) {
+            response.send(`${login_username} is logged in`);
+        } else {
+            response.send(`incorrect password for ${login_username}`);
+        }
+    } else {
+        response.send(`${login_username} does not exist`);
+
+    }
+    response.send('processing request' + JSON.stringify(request.body));
+});
+
+app.listen(8080, () => console.log(`listening on port 8080`));
