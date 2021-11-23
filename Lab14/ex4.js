e4e5cd18e3ce97f91847833ee01c1cf21bb64fc9
E-Melchor <emi96765@hawaii.edu>
2021-11-22 23:08:36 -1000

    Lab14

--- Lab14/ex4.js
+++ Lab14/ex4.js
@@ -14,6 +14,15 @@ if (fs.existsSync(filename)) {
     let data_str = fs.readFileSync(filename, 'utf-8');
     var user_registration_info = JSON.parse(data_str);
     console.log(user_registration_info);
+
+    //add new user data
+    username = 'newuser';
+    user_registration_info[username] = {};
+    user_registration_info[username].password = 'newpass';
+    user_registration_info[username].email = 'newuser@user.com';
+    let new_user_data = fs.readFileSync(filename, 'utf-8')
+    var user_registration_info = JSON.parse(new_user_data);
+
 } else {
     console.log(`Hey! ${filename} does not exist!`);
 }
@@ -25,6 +34,7 @@ app.get("/", function(request, response) {
 //data from form will be decoded
 app.use(express.urlencoded({ extended: true }));
 
+//create login form
 app.get("/login", function(request, response) {
     // Give a simple login form
     str = `
@@ -39,8 +49,9 @@ app.get("/login", function(request, response) {
     response.send(str);
 });
 
+//Process login form
 app.post("/login", function(request, response) {
-    // Process login form POST and redirect to logged in page if ok, back to login page if not
+    // Redirect to logged in page if ok, back to login page if not
     let login_username = request.body['username'];
     let login_password = request.body['password'];
     //check if username exists, then check password entered matched password stored
@@ -56,6 +67,29 @@ app.post("/login", function(request, response) {
 
     }
     response.send('processing request' + JSON.stringify(request.body));
+});
+
+//create registration form
+app.get("/register", function(request, response) {
+    // Give a simple register form
+    str = `
+<body>
+<form action="" method="POST">
+<input type="text" name="username" size="40" placeholder="enter username" ><br />
+<input type="password" name="password" size="40" placeholder="enter password"><br />
+<input type="password" name="repeat_password" size="40" placeholder="enter password again"><br />
+<input type="email" name="email" size="40" placeholder="enter email"><br />
+<input type="submit" value="Submit" id="submit">
+</form>
+</body>
+    `;
+    response.send(str);
+});
+
+app.post("/register", function(request, response) {
+    // process a simple register form
+    response.send('adding new user' + JSON.stringify(request.body));
+    console.log(user_registration_info);
 });
 
 app.listen(8080, () => console.log(`listening on port 8080`));
